2196e69d6f97ade0869bfc67b9a2df56b7fefc6d
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-04-09 03:16:57 +0200

    Add binary_tree_preorder file

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * @tree: pointer to a binary_tree_t struct - tree to browse
+ * @func: function pointer - callback function to use when browsing
+ * Return: void
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+}
